8015efb32f34652e9b89cf4320f9ad4b5c5ecade
Lionel EKOM <ekomlionel@gmail.com>
2024-05-26 20:47:58 +0100

    "Updated database settings from SQLite to PostgreSQL"

--- DocBlog/settings.py
+++ DocBlog/settings.py
@@ -79,8 +79,13 @@ WSGI_APPLICATION = 'DocBlog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'docuser',
+        'PASSWORD': '021121',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
